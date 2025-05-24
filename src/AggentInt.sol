337e768056257ab166f1cb538fa7cf8ec9380581
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-05-24 10:53:22 -0300

    feat: addition of getPriceInUniswap

--- src/AggentInt.sol
+++ src/AggentInt.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.13;
 
 import "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
 contract AgentInt {
     struct PriceFeedInfo {
@@ -23,7 +25,41 @@ contract AgentInt {
         feeds.push(PriceFeedInfo("JPY/USD", AggregatorV3Interface(0xf8B283aD4d969ECFD70005714DD5910160565b94)));
     }
 
-    function getPrice (string memory _pair) public view returns(uint) {
+    function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
+        (address token0, address token1) = sortTokens(tokenA, tokenB);
+        pair = address(uint160(uint256(keccak256(abi.encodePacked(
+            hex"ff",
+            factory,
+            keccak256(abi.encodePacked(token0, token1)),
+            hex"96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f" // init code hash UniswapV2
+        )))));
+    }
+
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        require(tokenA != tokenB, "Identical addresses");
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), "Zero address");
+    }
+
+    function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
+        (address token0,) = sortTokens(tokenA, tokenB);
+        address pair = pairFor(factory, tokenA, tokenB);
+        (uint reserve0, uint reserve1,) = IUniswapV2Pair(pair).getReserves();
+        (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+    }
+
+    function getPriceInUniswap(address tokenA, address tokenB, address factory) external view returns (uint256 price) {
+        address pair = pairFor(factory, tokenA, tokenB);
+        require(pair != address(0), "Pair does not exist");
+
+        (uint reserveA, uint reserveB) = getReserves(factory, tokenA, tokenB);
+        require(reserveA > 0 && reserveB > 0, "No reserves");
+
+        // preço de tokenB em tokenA (quantidade tokenB por 1 tokenA)
+        price = reserveB * 1e18 / reserveA;
+    }
+
+    function getMediumPrice (string memory _pair) public view returns(uint) {
         for (uint i = 0; i < feeds.length; i++) {
             if (keccak256(bytes(_pair)) == keccak256(bytes(feeds[i].pair))) {
                 (
@@ -56,7 +92,7 @@ contract AgentInt {
 
     function makeAnalysis (string memory _pair) public payable returns (string memory) {
 
-        // suzaku vai verificar o preço em 3 dex e olhar o gás
+        // avax vai verificar o preço em 3 dex e olhar o gás
 
         // verifica na chainlink se ta bom negócio pra fazer
 
